ab4f243d18d6a09a5aaac21141e6b2b2466f4208
hadasamuels <94486989+hadasamuels@users.noreply.github.com>
2022-01-12 21:58:04 +0200

    Update limitedRecord.py

--- limitedRecord.py
+++ limitedRecord.py
@@ -2,7 +2,7 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 from speechRecognition import recognition
 
-#the function enables recording, transfers the record to WAV file, and then by recognition function converts the WAV file to text file
+#The function enables recording, transfers the record to WAV file, and then by recognition function converts the WAV file to text file
 def rec():
 	fs = 44100  # Sample rate
 	seconds = 10  # Duration of recording
@@ -12,12 +12,12 @@ def rec():
 	sd.wait()  # Wait until recording is finished
 	write('output.wav', fs, myrecording)  # Save as WAV file
 
-	text=recognition('output.wav') #uses the recognition function from SpeechRecognition.py to convert the WAV file to text
+	text=recognition('output.wav') #Uses the recognition function from SpeechRecognition.py to convert the WAV file to text
 	f=open("text.txt","w")
 	try:
-		f.write(text) #write the text to file
+		f.write(text) #Write the text to file
 	except:
-		print(1) #an error occured
+		print(1) #An error occured
 	f.close()
 
 
