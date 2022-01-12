584129accf12d86595539e7b926a047d29588fdd
hadasamuels <94486989+hadasamuels@users.noreply.github.com>
2022-01-12 21:52:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,24 +16,25 @@ Window.size = (400, 500)
 
 #The login screen department that includes functions related to logging in to the system
 class Login(Screen):
-    #The login function - receives a username and password from the Gui
+    #The login function - receives a username and password from the GUI
     def do_login(self, loginText, passwordText):
         app = App.get_running_app()
-        app.username = loginText #insert the user name text into the appropriate variable
-        app.password = passwordText #insert the password text into the appropriate variable
+        app.username = loginText #Insert the user name text into the appropriate variable
+        app.password = passwordText #Insert the password text into the appropriate variable
         names = {"user": "12345"}
 
         if app.username in names.keys() and app.password == names[app.username]: #Check whether the username and password entered in the system are correct and authorized to enter
             self.manager.transition = SlideTransition(direction='right')
             self.manager.current = 'times'
-        else: #issues an error message that it is not possible to log in because a username or password is incorrect / unauthorized
+        else: #Issues an error message that it is not possible to log in because a username or password is incorrect / unauthorized
             self.manager.transition = SlideTransition(direction='right')
             self.manager.current = 'error'
 
         app.config.read(app.get_application_config())
         app.config.write()
-
-    def resetForm(self): #rester function to the  user name and password fields
+        
+#The function resets the user name and password fields
+    def resetForm(self): 
         self.ids['login'].text = ""
         self.ids['password'].text = ""
 
@@ -43,7 +44,7 @@ class LoginApp(MDApp):
     password = StringProperty(None)
     dialog=None
     
-    #function Build program windows so we can access them from the main window and grab the desired window each time
+    #Function Build program windows so we can access them from the main window and grab the desired window each time
     def build(self):
         manager = ScreenManager()
 
@@ -58,6 +59,7 @@ class LoginApp(MDApp):
         manager.add_widget(birth(name='birth'))
         return manager
 
+    #Function to get the application's configuration 
     def get_application_config(self):
         if not self.username:
             return super(LoginApp, self).get_application_config()
@@ -69,53 +71,53 @@ class LoginApp(MDApp):
 
         return super(LoginApp, self).get_application_config('%s/config.cfg' % conf_directory)
     
-    #function responsible for ending the program and doing things in parallel with the end of the run
+    #Function responsible for ending the program and doing things in parallel with the end of the run
     def end_button_press(self):
 
         screenList=['demographic','measures1','measures2','medicalHistory']
-        f=open("summaryReport.txt","w") #file to fill in the details from the fields and transfer them to a file to send
+        f=open("summaryReport.txt","w") #Summary file containing the details from all the fields, will be sent by email
 
-        for id in self.root.get_screen('times').ids: #loop that passes through the fields of times
+        for id in self.root.get_screen('times').ids: #Go through the fields of times screen
             if 'Label' not in id:
                 field = self.root.get_screen('times').ids[id+"Label"].text[::-1]
                 print(field)
                 value = self.root.get_screen('times').ids[id].text
-                if value != "": #check if all the fields of the times are full
+                if value != "": #Check if all the fields of the times are full
                     f.write(field + ":" + value + "\n")
-                else: #issues an error message that it is necessary to fill in the blanks
+                else: #Issues an error message that it is necessary to fill in the blanks
                     self.show_alert_dialog()
                     return
 
-        for screen in screenList: #loop that goes over the fields and outputs the field values to transfer the details to the file to be sent
-            for id in self.root.get_screen(screen).ids:
-                field=self.root.get_screen(screen).ids[id].hint_text[::-1] #turns the text because it's in Hebrew
+        for screen in screenList: #Go through the fields and write their values (details) to the summary file that will be sent
+            for id in self.root.get_screen(screen).ids: 
+                field=self.root.get_screen(screen).ids[id].hint_text[::-1] #Reverse the text because it's in Hebrew
                 value=self.root.get_screen(screen).ids[id].text[::-1]
-                if value!="" or screen=='birth' or screen=='medicalHistory': #check if the fields are not rake and gives the possibility to continue the program even if it is spoken on the Lida screen or medical history because it is not mandatory to fill
-                    if value.isdigit(): #turns the text if it is a digit 
+                if value!="" or screen=='birth' or screen=='medicalHistory': #Check if the fields are not empty and gives the possibility to continue the program even if it is spoken on the birth screen or medical history because it is not mandatory to fill
+                    if value.isdigit(): #Turns the text if it is a digit 
                         value=value[::-1]
                     f.write(field+":"+value+"\n")
-                else: #issues an error message that the fields are required to be filled in
+                else: #Issues an error message that the fields are required to be filled in
                     self.show_alert_dialog()
                     return
 
         f.close()
-        send_mail() #send the form by email
+        send_mail() #Send the form by email
         self.reset_all()
         return
     
-    #function that initializes the fields so that they can use another event without exiting the program
+    #Function that initializes the fields so that they can use another event without exiting the program
     def reset_all(self): 
         screenList = ['demographic', 'measures1', 'measures2', 'medicalHistory','birth']
 
-        for id in self.root.get_screen('times').ids:
+        for id in self.root.get_screen('times').ids: #Go through the times
             if 'Label' not in id:
                 self.root.get_screen('times').ids[id].text="העש דעת"
 
-        for screen in screenList:
+        for screen in screenList: #Go through the screens
             for id in self.root.get_screen(screen).ids:
                 self.root.get_screen(screen).ids[id].text=""
 
-    #alert Windows function
+    #Alert windows function
     def show_alert_dialog(self):
         if not self.dialog:
             self.dialog = MDDialog(
@@ -133,11 +135,12 @@ class LoginApp(MDApp):
             )
 
         self.dialog.open()
-
+    
+    #Close the alert 
     def close_dialog(self,obj):
         self.dialog.dismiss()
 
 
-#start the program run
+#Run the program
 if __name__ == '__main__':
     LoginApp().run()
