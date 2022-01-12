5ac9e1f7163c2bf721d4b9e2ef0c5f1e75b3a561
hadasamuels <94486989+hadasamuels@users.noreply.github.com>
2022-01-12 21:27:56 +0200

    Update speechRecognition.py

--- speechRecognition.py
+++ speechRecognition.py
@@ -1,12 +1,14 @@
 #SpeechRecognition
 import speech_recognition as sr
+
+#the function receives a WAV file, and convert it to text using Speech Recognition
 def recognition(file):
-    r = sr.Recognizer()
-    audiofile = sr.AudioFile(file)
+    r = sr.Recognizer() #create a Recognizer object
+    audiofile = sr.AudioFile(file) #convert the WAV file to AudioFile
     with audiofile as source:
-        clip = r.record(source)
-    s = r.recognize_google(clip, language="he-IL")
+        clip = r.record(source) #convert the AudioFile file to DataFile using record function
+    s = r.recognize_google(clip, language="he-IL") #convert the DataFile to text using the recognize_google function
     f = open("text_from_speech.txt", "w")
-    f.write(str(s))
+    f.write(str(s)) #write the text to file
     print(s)
     return
